bfc2ee962522fcf5b7302dc3148fca11ce8ba210
Chan Yew Yen <chan.yewyen@gmail.com>
2021-12-08 11:32:02 +0800

    Add tests for setting interest and duration

--- bank/src/test/java/ibf2021/d3/FixedDepositAccountTest.java
+++ bank/src/test/java/ibf2021/d3/FixedDepositAccountTest.java
@@ -1,6 +1,7 @@
 package ibf2021.d3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -67,4 +68,58 @@ public class FixedDepositAccountTest {
         account.withdraw(-50f);
         assertEquals(initialBalance, account.getBalance(), DELTA);
     }
+
+    @Test
+    public void changeInterest_ChangeInterestOnce_ShouldChangeInterest() {
+        FixedDepositAccount account = new FixedDepositAccount("Jar Jar Binks", 100f, 5f, 12);
+        float initialInterest = account.getInterest();
+        account.setInterest(initialInterest + 5);
+        float interest = account.getInterest();
+        assertEquals(initialInterest + 5, interest, DELTA);
+    }
+
+    @Test
+    public void changeInterest_ChangeInterestMultipleTimes_ShouldThrowIllegalArgumentException() {
+        FixedDepositAccount account = new FixedDepositAccount("Jar Jar Binks", 100f, 5f, 12);
+        float initialInterest = account.getInterest();
+        account.setInterest(initialInterest + 5);
+        float interest = account.getInterest();
+        assertEquals(initialInterest + 5, interest, DELTA);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            account.setInterest(interest + 5);
+        });
+        System.out.println(exception.getMessage());
+    }
+
+    @Test
+    public void changeDuration_ChangeDurationOnce_ShouldChangeDuration() {
+        FixedDepositAccount account = new FixedDepositAccount("Jar Jar Binks", 100f, 5f, 12);
+        int initialDuration = account.getDuration();
+        account.setDuration(initialDuration + 5);
+        int duration = account.getDuration();
+        assertEquals(initialDuration + 5, duration);
+    }
+
+    @Test
+    public void changeDuration_ChangeDurationMultipleTimes_ShouldThrowIllegalArgumentException() {
+        FixedDepositAccount account = new FixedDepositAccount("Jar Jar Binks", 100f, 5f, 12);
+        int initialDuration = account.getDuration();
+        account.setDuration(initialDuration + 6);
+        int duration = account.getDuration();
+        assertEquals(initialDuration + 6, duration);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            account.setDuration(duration + 6);
+        });
+        System.out.println(exception.getMessage());
+    }
+
+   /*  // this test is no longer needed because we no longer have access to setBalance from child class
+    @Test
+    public void setBalance_ChangeBalanceAfterAccountCreation_ShouldNotChangeBalance() {
+        FixedDepositAccount account = new FixedDepositAccount("Jar Jar Binks", 100f, 5f, 12);
+        float initialBalance = account.getBalance();
+        account.setBalance(initialBalance + 200f);
+        float balance = account.getBalance();
+        assertEquals(initialBalance, balance);
+    } */
 }
